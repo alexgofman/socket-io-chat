145744cf38088f8927f8cdb0790a8f3d17bf04f3
Alex Gofman <gofmanalex13@gmail.com>
2016-02-24 00:15:23 +0400

    finilized simple operating chat app

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ io.on('connection', function(socket){
   });
 });
 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
 http.listen(1337, function() {
   console.log('Server is listening on port: 1337');
 });
+
+
+// TODO: THINGS TO IMPOVE:
+//
+// Broadcast a message to connected users when someone connects or disconnects
+// Add support for nicknames
+// Don’t send the same message to the user that sent himself. Instead, append the message directly as soon as he presses enter.
+// Add “{user} is typing” functionality
+// Show who’s online
+// Add private messaging
+//
